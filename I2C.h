8728dc447975e42fe6db3ae8d9a19b4165496722
FilipPolednia <filip.polednia@gmail.com>
2017-02-09 20:47:42 +0100

    Update I2C.h

--- I2C.h
+++ I2C.h
@@ -6,12 +6,17 @@ typedef enum{
   I2C_NACK = 1,
 } I2C_Acknowledge_Bit;
 
-void    i2c_Disable(I2C_Type* i2c);
-void    i2c_Disable_Interrupt(I2C_Type* i2c);
-void    i2c_Enable(I2C_Type* i2c);
-void    i2c_Enable_Interrupt(I2C_Type* i2c);
-uint8_t i2c_ReadByte(I2C_Type* i2c, uint8_t ack);
-void    i2c_Restart(I2C_Type* i2c);
-void    i2c_Start(I2C_Type* i2c);
-void    i2c_Stop(I2C_Type* i2c);
-uint8_t i2c_WriteByte(I2C_Type* i2c, uint8_t data);
+void	I2C0_Init();
+void    I2C_Disable(I2C_Type* i2c);
+void    I2C_DisableInt(I2C_Type* i2c);
+void    I2C_Enable(I2C_Type* i2c);
+void    I2C_EnableInt(I2C_Type* i2c);
+uint8_t I2C_ReadByte(I2C_Type* i2c, uint8_t ack);
+void    I2C_Restart(I2C_Type* i2c);
+void    I2C_Start(I2C_Type* i2c);
+void    I2C_Stop(I2C_Type* i2c);
+uint8_t I2C_WriteByte(uint8_t data);
+char	I2C_ReadRegister(unsigned char u8SlaveAddress, unsigned char u8RegisterAddress);
+void 	I2C_WriteRegister(unsigned char u8SlaveAddress, unsigned char u8RegisterAddress, /*unsigned*/ char u8Data);
+void 	I2C_ReadMultiRegisters(unsigned char u8SlaveAddress, unsigned char u8RegisterAddress, unsigned char n, unsigned char *r);
+void 	I2C0_IRQHandler();
