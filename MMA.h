30cbd330b9c0b5d4bcee67d1044935b190951c60
mmmKaczmar <mmm.kaczmar@gmail.com>
2017-02-13 18:43:40 +0100

    Update MMA.h

--- MMA.h
+++ MMA.h
@@ -1,2 +1,14 @@
+/////////////////////////////////////////////
+// Systemy Mikroprocesorowe II             //
+// Projekt zaliczeniowy                    //
+// Filip Polednia, Mateusz Kaczmarczyk     //
+// Elektronika III                         //
+// AGH                                     //
+/////////////////////////////////////////////
+
 void Accelerometer_Init (void);
 void Calibrate (void);
+
+//////////////////////////////////
+//             eof              //
+//////////////////////////////////
